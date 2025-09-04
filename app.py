180b8a7ab5413275262ea054d48b6cb459cfa698
prikl11 <119262114+prikl11@users.noreply.github.com>
2025-09-04 18:58:13 +0300

    Add payments router

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from database import Base, engine
-from routers import users, admins, accounts
+from routers import users, admins, accounts, payments
 
 app = FastAPI()
 
@@ -9,3 +9,4 @@ Base.metadata.create_all(bind=engine)
 app.include_router(users.router, tags=["users"])
 app.include_router(admins.router, tags=["admins"])
 app.include_router(accounts.router, tags=["accounts"])
+app.include_router(payments.router, tags=["payments"])
